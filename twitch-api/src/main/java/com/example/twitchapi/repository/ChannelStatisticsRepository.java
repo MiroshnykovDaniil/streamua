e04da2676a9380602ea50dd47878568415cd851e
danilmiroshnikov <danielq911q@gmail.com>
2021-08-17 15:06:16 +0300

    ChannelStatisticsRepository ChannelStatisticsService

---
+++ twitch-api/src/main/java/com/example/twitchapi/repository/ChannelStatisticsRepository.java
@@ -0,0 +1,9 @@
+package com.example.twitchapi.repository;
+
+import com.example.twitchapi.model.ChannelStatistics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelStatisticsRepository extends JpaRepository<ChannelStatistics,String> {
+
+
+}
---
+++ twitch-api/src/main/java/com/example/twitchapi/service/ChannelStatisticsService.java
@@ -0,0 +1,8 @@
+package com.example.twitchapi.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChannelStatisticsService {
+}
